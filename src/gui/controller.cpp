55b66acd6896f66efc5dd2c7fc5cbeb69538f658
Sergio Martins <@>
2025-11-13 16:04:13 -0500

    feat: Added week navigation features to controller

--- src/gui/controller.cpp
+++ src/gui/controller.cpp
@@ -3,14 +3,58 @@
 
 #include "controller.h"
 
+namespace {
+QDate firstMondayOfWeek(const QDate &date)
+{
+    return date.addDays(1 - date.dayOfWeek());
+}
+}
+
 Controller::Controller(QObject *parent)
     : QObject(parent)
 {
+    navigatorGotoToday();
 }
+
 
 Controller::ViewType Controller::currentViewType() const
 {
     return _currentViewType;
+}
+
+QDate Controller::navigatorStartDate() const
+{
+    return _navigatorStartDate;
+}
+
+QDate Controller::navigatorEndDate() const
+{
+    return _navigatorStartDate.addDays(6);
+}
+
+
+void Controller::setNavigatorStartDate(const QDate &date)
+{
+    if (_navigatorStartDate == date)
+        return;
+    _navigatorStartDate = date;
+    emit navigatorStartDateChanged();
+    emit navigatorEndDateChanged();
+}
+
+void Controller::navigatorGotoToday()
+{
+    setNavigatorStartDate(firstMondayOfWeek(QDate::currentDate()));
+}
+
+void Controller::navigatorGotoNextWeek()
+{
+    setNavigatorStartDate(_navigatorStartDate.addDays(7));
+}
+
+void Controller::navigatorGotoPreviousWeek()
+{
+    setNavigatorStartDate(_navigatorStartDate.addDays(-7));
 }
 
 void Controller::setCurrentViewType(ViewType viewType)
--- src/gui/controller.h
+++ src/gui/controller.h
@@ -3,6 +3,7 @@
 
 #pragma once
 
+#include <QDate>
 #include <QObject>
 #include <QtQml/qqmlregistration.h>
 
@@ -11,6 +12,8 @@ class Controller : public QObject {
     QML_ELEMENT
     QML_SINGLETON
     Q_PROPERTY(Controller::ViewType currentViewType READ currentViewType WRITE setCurrentViewType NOTIFY currentViewTypeChanged)
+    Q_PROPERTY(QDate navigatorStartDate READ navigatorStartDate NOTIFY navigatorStartDateChanged)
+    Q_PROPERTY(QDate navigatorEndDate READ navigatorEndDate NOTIFY navigatorEndDateChanged)
 
 public:
     enum class ViewType {
@@ -25,9 +28,19 @@ public:
     ViewType currentViewType() const;
     void setCurrentViewType(ViewType viewType);
 
+    QDate navigatorStartDate() const;
+    QDate navigatorEndDate() const;
+    void setNavigatorStartDate(const QDate& date);
+    Q_INVOKABLE void navigatorGotoToday();
+    Q_INVOKABLE void navigatorGotoNextWeek();
+    Q_INVOKABLE void navigatorGotoPreviousWeek();
+
 Q_SIGNALS:
     void currentViewTypeChanged();
+    void navigatorStartDateChanged();
+    void navigatorEndDateChanged();
 
 private:
     ViewType _currentViewType = ViewType::Week;
+    QDate _navigatorStartDate;
 };
