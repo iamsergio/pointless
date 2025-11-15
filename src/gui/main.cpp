d0cd6e04f4c3415f1df2c48889d669adc30d71bf
Sergio Martins <@>
2025-11-15 16:58:33 -0500

    Use Fusion style across all platforms
    native styles can't be customized

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -3,9 +3,9 @@
 
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
 #include <QDirIterator>
 #include <QDebug>
+#include <QQuickStyle>
 
 void printQrcContents(const QString &path = ":/", int depth = 0)
 {
@@ -27,6 +27,7 @@ void printQrcContents(const QString &path = ":/", int depth = 0)
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    QQuickStyle::setStyle("Fusion");
     QQmlApplicationEngine engine;
 
     engine.loadFromModule("pointless", "Main");
