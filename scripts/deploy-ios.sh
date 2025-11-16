e2e22bca8aeac0d61a7bb9092246384dab553ae5
Sergio Martins <@>
2025-11-16 17:11:49 -0500

    ios: improve deploy script

--- scripts/deploy-ios.sh
+++ scripts/deploy-ios.sh
@@ -7,6 +7,26 @@
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+BIN_DIR="Release"
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+	--debug)
+		BIN_DIR="Debug"
+		shift
+		;;
+	*)
+		echo "Unknown argument: $1" >&2
+		echo "Usage: $0 [--debug]" >&2
+		exit 1
+		;;
+	esac
+done
+
+if [[ -z "${IPHONE_DEVICE_ID:-}" ]]; then
+	echo "IPHONE_DEVICE_ID is not set" >&2
+	exit 1
+fi
+
 cd "${SCRIPT_DIR}"/../build-ios-dev/
 
-xcrun devicectl device install app --device "$IPHONE_DEVICE_ID" ./bin/Release/pointless.app
+xcrun devicectl device install app --device "$IPHONE_DEVICE_ID" ./bin/"${BIN_DIR}"/pointless.app
