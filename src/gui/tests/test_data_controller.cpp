13f49e495265ae54f16b551831f6482f8d8175ff
Sergio Martins <@>
2025-12-27 22:18:25 +0000

    Add test_data_controller.cpp

---
+++ src/gui/tests/test_data_controller.cpp
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2025 Sergio Martins
+// SPDX-License-Identifier: MIT
+
+#include "gui/data_controller.h"
+#include "core/context.h"
+#include "core/data_provider.h"
+
+#include <gtest/gtest.h>
+
+TEST(DataControllerTest, ConstructDestroy)
+{
+    pointless::core::Context::setContext({ IDataProvider::Type::TestSupabase, "/tmp/nonexistent_file.json" });
+    DataController controller;
+}
