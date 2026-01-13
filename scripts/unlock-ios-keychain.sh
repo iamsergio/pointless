e3887dda11686e231a2dee7ede07b0495d9af795
Sergio Martins <@>
2026-01-13 23:45:42 +0000

    ios: Add script to unlock keychain

---
+++ scripts/unlock-ios-keychain.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2025 Sergio Martins
+#
+# SPDX-License-Identifier: MIT
+
+
+security show-keychain-info "${KEYCHAIN_FILE}" 2>&1 | grep -q "User interaction is not allowed"
+if [ $? -ne 0 ]; then
+    echo "Keychain is already unlocked."
+    exit 0
+fi
+
+if [ -z "${KEYCHAIN_FILE}" ]; then
+    echo "KEYCHAIN_FILE environment variable must be set"
+    exit 1
+fi
+
+if [ -z "${KEYCHAIN_PASSWORD}" ]; then
+	echo "KEYCHAIN_PASSWORD environment variable not set, please enter password:"
+	read -rs KEYCHAIN_PASSWORD
+fi
+
+security unlock-keychain -p "${KEYCHAIN_PASSWORD}" "${KEYCHAIN_FILE}"
