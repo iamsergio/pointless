a2d1c176f2bf241b9b88d0ecea689f98922d0e29
Sergio Martins <@>
2026-01-17 19:39:45 +0000

    ios: publish to testflight

--- scripts/github-ios-set-secrets.sh
+++ scripts/github-ios-set-secrets.sh
@@ -28,6 +28,21 @@ if [ -z "${APPLE_TEAM_ID}" ]; then
 	exit 1
 fi
 
+if [ -z "${APPLE_SPECIFIC_PASSWORD}" ]; then
+	echo "APPLE_SPECIFIC_PASSWORD environment variable must be set"
+	exit 1
+fi
+
+if [ -z "${POINTLESS_APPLE_ID}" ]; then
+	echo "POINTLESS_APPLE_ID environment variable must be set"
+	exit 1
+fi
+
+if [ -z "${POINTLESS_APP_APPLE_ID}" ]; then
+	echo "POINTLESS_APP_APPLE_ID environment variable must be set"
+	exit 1
+fi
+
 if ! gh auth status >/dev/null 2>&1; then
 	echo "Not authenticated. Starting login..."
 	gh auth login
@@ -38,8 +53,7 @@ fi
 base64 -i "${APPLE_CERTIFICATE_P12_PATH}" | gh secret set APPLE_CERTIFICATE_P12_BASE64
 base64 -i "${APPLE_PROVISIONING_PROFILE_PATH}" | gh secret set APPLE_PROVISIONING_PROFILE_BASE64
 
-echo "Enter KEYCHAIN_PASSWORD:"
-read -rs KEYCHAIN_PASSWORD
+KEYCHAIN_PASSWORD=$(openssl rand -base64 16) # not really important, just a random password
 echo -n "${KEYCHAIN_PASSWORD}" | gh secret set KEYCHAIN_PASSWORD
 
 echo "Enter APPLE_P12_PASSWORD:"
@@ -48,3 +62,6 @@ echo -n "${APPLE_P12_PASSWORD}" | gh secret set APPLE_P12_PASSWORD
 
 echo -n "${APPLE_PROVISIONING_PROFILE_UUID}" | gh secret set APPLE_PROVISIONING_PROFILE_UUID
 echo -n "${APPLE_TEAM_ID}" | gh secret set APPLE_TEAM_ID
+echo -n "${APPLE_SPECIFIC_PASSWORD}" | gh secret set APPLE_SPECIFIC_PASSWORD
+echo -n "${POINTLESS_APPLE_ID}" | gh secret set POINTLESS_APPLE_ID
+echo -n "${POINTLESS_APP_APPLE_ID}" | gh secret set POINTLESS_APP_APPLE_ID
