487541877184ae14879386dff9f0e9c6923806c1
Sergio Martins <@>
2026-01-26 13:39:53 +0000

    macos: add deploy script

---
+++ scripts/deploy-macos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2025 Sergio Martins
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "${SCRIPT_DIR}"/../
+
+PRESET=rel
+DEST=${PUB_DATA_DIR}/etc/bin/
+
+cmake --preset=${PRESET}
+cmake --build build-${PRESET}
+cp build-${PRESET}/bin/pointless ${DEST}
+
+echo
+echo "Deployed pointless to ${DEST}"
