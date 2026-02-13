35ea31445f78cd7a9994fc6fc9c6be2f0886263b
Sergio Martins <@>
2026-02-13 20:56:51 +0000

    add package-macos.sh

---
+++ scripts/package-macos.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+ARCH="arm64"
+LIPO="x86_64"
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <build_dir>"
+    exit 1
+fi
+
+BUILD_DIR=$1
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${BUILD_DIR}"
+
+${SCRIPT_DIR}/sign-macos.sh
+
+VERSION=$(cat version.txt)
+
+pkgbuild --root Pointless.app \
+     --identifier com.iamsergio.pointless \
+     --version ${VERSION} \
+     --install-location /Applications/Pointless.app \
+     Pointless-unsigned.pkg && \
+# productsign --sign $APPLE_DEV_ID_INSTALLER Pointless-unsigned.pkg Pointless.pkg && \
+# xcrun notarytool submit  Pointless.pkg --apple-id iamserj@icloud.com --team-id $APPLE_TEAM_ID --password $APPLE_SPECIFIC_PASSWORD --wait && \
+# xcrun stapler staple -v Pointless.pkg && \
+# cd .. && \
+
