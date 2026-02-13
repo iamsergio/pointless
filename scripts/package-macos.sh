1a82ca1639cbc23071465d1d0862683e53a85c7e
Sergio Martins <@>
2026-02-13 20:58:38 +0000

    add package-macos.sh

--- scripts/package-macos.sh
+++ scripts/package-macos.sh
@@ -10,6 +10,11 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
+if [ -z "$APPLE_TEAM_ID" ] || [ -z "$APPLE_ID" ] || [ -z "$APPLE_SPECIFIC_PASSWORD" ] || [ -z "$APPLE_DEV_ID_INSTALLER" ]; then
+    echo "Error: Required environment variables not set (APPLE_TEAM_ID, APPLE_ID, APPLE_SPECIFIC_PASSWORD, APPLE_DEV_ID_INSTALLER)"
+    exit 1
+fi
+
 BUILD_DIR=$1
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -23,9 +28,9 @@ pkgbuild --root Pointless.app \
      --identifier com.iamsergio.pointless \
      --version ${VERSION} \
      --install-location /Applications/Pointless.app \
-     Pointless-unsigned.pkg && \
+     Pointless-unsigned.pkg #&& \
 # productsign --sign $APPLE_DEV_ID_INSTALLER Pointless-unsigned.pkg Pointless.pkg && \
-# xcrun notarytool submit  Pointless.pkg --apple-id iamserj@icloud.com --team-id $APPLE_TEAM_ID --password $APPLE_SPECIFIC_PASSWORD --wait && \
+# xcrun notarytool submit  Pointless.pkg --apple-id ${APPLE_ID} --team-id $APPLE_TEAM_ID --password $APPLE_SPECIFIC_PASSWORD --wait && \
 # xcrun stapler staple -v Pointless.pkg && \
 # cd .. && \
 
