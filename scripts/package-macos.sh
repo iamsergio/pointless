c9c7e85c0769e5559405602013c55e942e7b8460
Sergio Martins <@>
2026-02-13 22:41:45 +0000

    macOS: Notarize package

--- scripts/package-macos.sh
+++ scripts/package-macos.sh
@@ -18,19 +18,17 @@ fi
 BUILD_DIR=$1
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "${BUILD_DIR}"
 
-${SCRIPT_DIR}/sign-macos.sh
+${SCRIPT_DIR}/sign-macos.sh $BUILD_DIR
 
 VERSION=$(cat version.txt)
 
-pkgbuild --root Pointless.app \
+cd "${BUILD_DIR}"
+pkgbuild --root bin/pointless.app \
      --identifier com.iamsergio.pointless \
      --version ${VERSION} \
      --install-location /Applications/Pointless.app \
-     Pointless-unsigned.pkg #&& \
-# productsign --sign $APPLE_DEV_ID_INSTALLER Pointless-unsigned.pkg Pointless.pkg && \
-# xcrun notarytool submit  Pointless.pkg --apple-id ${APPLE_ID} --team-id $APPLE_TEAM_ID --password $APPLE_SPECIFIC_PASSWORD --wait && \
-# xcrun stapler staple -v Pointless.pkg && \
-# cd .. && \
-
+     Pointless-unsigned.pkg && \
+ productsign --sign $APPLE_DEV_ID_INSTALLER Pointless-unsigned.pkg Pointless.pkg && \
+xcrun notarytool submit  Pointless.pkg --apple-id ${APPLE_ID} --team-id $APPLE_TEAM_ID --password $APPLE_SPECIFIC_PASSWORD --wait && \
+xcrun stapler staple -v Pointless.pkg
