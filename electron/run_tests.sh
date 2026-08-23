ccd2baf0c0ba7873c2f34ae839d9bdc869483397
Sergio Martins <@>
2026-08-23 13:54:05 +0100

    Remove electron

--- electron/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$SCRIPT_DIR"
-rm -rf test-results/screenshots/*
-npm run build
-npm test
-npm run test:core
-npm run test:e2e
